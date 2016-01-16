f16e3e8356a5bd697a59d71cd5639e6dc7eccdfe
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2016-01-16 16:26:28 +0100

    devise user

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# setup advice devise
+# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
