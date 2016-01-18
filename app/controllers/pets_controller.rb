72a944469ef7d36fe79b4989533c738e24ad1911
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2016-01-18 14:35:40 +0100

    edit and delete pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,20 +1,24 @@
 class PetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pets = Pet.all
   end
 
   def new
-    @user = current_user
+    @user = User.find_by_id(params[:user_id])
     @pet = @user.pets.new
+    unless @user == current_user
+      redirect_to root_path, :alert => "Access denied."
+    end
   end
 
   def create
-    @user = current_user
-    @pet = @user.pets.create(pet_params)
-    if @pet
+    @user = User.find_by_id(params[:user_id])
+    @pet = @user.pets.new(pet_params)
+    if @pet.save && @user == current_user
       flash[:notice] = 'Pet added successfully'
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       flash[:alert] = "Sorry, something went wrong."
       render 'new'
@@ -22,9 +26,37 @@ class PetsController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find_by_id(params[:user_id])
+    @pet = Pet.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find_by_id(params[:user_id])
+    @pet = Pet.find_by_id(params[:id])
+    if @pet.update_attributes(pet_params) && @user == current_user
+      flash[:notice] = 'Pet added successfully'
+    else
+      flash[:alert] = "Sorry, something went wrong."
+    end
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @pet = Pet.find_by_id(params[:id])
+    if @pet.user_id == current_user.id
+      @pet.destroy
+      flash[:notice] = 'Pet deleted successfully'
+    else
+      flash[:alert] = "Sorry, something went wrong."
+    end
+    redirect_to user_path(current_user)
+  end
+
   private
 
   def pet_params
     params.require(:pet).permit(:name, :species, :age)
   end
+
 end
