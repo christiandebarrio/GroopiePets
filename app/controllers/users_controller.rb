66715c37eb8238e5e9e4e10b4fdb1191ca60aa43
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2016-01-18 14:37:32 +0100

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
+    @pets = @user.pets.all
     unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
+      redirect_to root_path, :alert => "Access denied."
     end
   end
 end
